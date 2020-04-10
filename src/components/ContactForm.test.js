61c02a4b3f312f031da3707880534642162205de
janecyyu <janecyyu@gmail.com>
2020-04-09 20:02:36 -0700

    add checkbox and the testing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, waitForDomChange } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
 //test if titles are correct
@@ -16,7 +16,6 @@ test("First Name, Last Name and Email are appearing", () => {
   expect(Email).toBeInTheDocument();
 });
 
-
 test("test to sure that the inputs can be filled in",() => {
   async () => {
     const { getByLabelText, findByTestId } = render(<ContactForm />);
@@ -24,10 +23,13 @@ test("test to sure that the inputs can be filled in",() => {
     const lastNameInput = getByLabelText(/Last/i);
     const emailInput = getByLabelText(/Email/i);
     const messageInput = getByLabelText(/Message/i);
+    const checkBoxInput = getByLabelText(/Agreement/i);
     fireEvent.change(firstNameInput, { target: { value: "hue" } });
     fireEvent.change(lastNameInput, { target: { value: "will" } });
     fireEvent.change(emailInput, { target: { value: "email@mail.com" } });
     fireEvent.change(messageInput, { target: { value: "greeting" } });
+    fireEvent.click(checkBoxInput);
+    expect(checkBoxInput).toBeTruthy();
     expect(firstNameInput.value).toBe("hue");
     expect(lastNameInput.value).toBe("will");
     expect(emailInput.value).toBe("email@mail.com");
